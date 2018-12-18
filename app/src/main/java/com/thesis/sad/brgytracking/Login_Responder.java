6a540ad152842acc3f72f49d0ce526ae2df1271f
joshuajetomo <joshuajetomo@ymail.com>
2018-12-18 21:55:52 +0800

    Brgy Register and Login Responder by joel

--- app/src/main/java/com/thesis/sad/brgytracking/Login_Responder.java
+++ app/src/main/java/com/thesis/sad/brgytracking/Login_Responder.java
@@ -5,21 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class Login_Responder extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button btn_responder=(Button) findViewById(R.id.btn_responder);
-        btn_responder.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_login__responder);
+        TextView btn_register = (TextView) findViewById(R.id.sign_up);
+        btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Login_Responder.class));
+                startActivity(new Intent(Login_Responder.this,brgy_register.class));
             }
         });
-
     }
 }
---
+++ app/src/main/java/com/thesis/sad/brgytracking/brgy_register.java
@@ -0,0 +1,13 @@
+package com.thesis.sad.brgytracking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class brgy_register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_brgy_register);
+    }
+}
