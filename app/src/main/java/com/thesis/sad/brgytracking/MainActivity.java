4f5dae1648db326ae1142c9c5a6326235aeb8adc
Joel <45962784+joelrostata@users.noreply.github.com>
2019-01-09 15:17:51 +0800

    Joel Toast

--- app/src/main/java/com/thesis/sad/brgytracking/MainActivity.java
+++ app/src/main/java/com/thesis/sad/brgytracking/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        Toast.makeText(this, "Hoy!", Toast.LENGTH_SHORT).show();
         getLocationPermission();
 
         button_user = findViewById(R.id.btn_user);
